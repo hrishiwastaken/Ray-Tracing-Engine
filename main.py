28e7a5d7a7aa7d49ed51c2c726aeaccb1f22d139
hrishiwastaken <hrishikesh.anilnair@gmail.com>
2026-08-09 14:06:37 +0530

    Added Refraction, Total Internal Reflection, Fixed Mirror Intersection detection bug, essentially finishing geometrical optics simulation
    Added Refraction, Total Internal Reflection, Fixed Mirror Intersection detection bug, essentially finishing geometrical optics simulation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Arc
+from matplotlib.widgets import TextBox
 import math
 from typing import List
 # Initializing Matplotlib
 fig, ax = plt.subplots(figsize=(8, 8))
+box_ax = None
+textbox = None
 
 # Function to reset and format the axes upon every click
 def setup_axes():
@@ -14,42 +17,84 @@ def setup_axes():
     ax.grid(True, linestyle='--', alpha=0.5)
     ax.set_title("Recursive Ray Tracing \n Left Click to move Source, Right Click to place mirror, Shift + Right Click to place inverted mirror, C to undo mirror")
 
-# The mirror object containing all mirror properties
+
 class Mirror:
-    def __init__(self, centre, radius, aperture, facing):
+    def __init__(self, centre, radius, aperture, facing, lens=False):
         self.centre = np.array(centre, dtype="float")
         self.radius = radius
         self.angle = aperture    
         self.facing = facing   
         self.centre_angle = 0 if self.facing == 1 else 180
+        self.lens = lens
     
     def draw(self):
-        if self.facing == 1:
-            t1 = -self.angle/2
-            t2 = self.angle/2
-        else:
-            t1 = 180 - self.angle/2
-            t2 = 180 + self.angle/2
-        
+        t1 = self.centre_angle - self.angle/2
+        t2 = self.centre_angle + self.angle/2        
         arc = Arc(
             tuple(self.centre), width=2*self.radius, height=2*self.radius,
             angle=0, theta1=t1,theta2=t2,
             linewidth=3, color='black'
         )
         ax.add_patch(arc)
-    
+
+  
     def normal(self, point) -> np.ndarray:
-        return self.centre - np.array(point, dtype="float")   # Uses vectoral analysis and properties of circle to compute normal.
+        return np.array(point, dtype="float") - self.centre   # Uses vectoral analysis and properties of circle to compute normal.
+
+# The Lens Object( Uses Mirrors as a helper class )
+class Lens:
+    def __init__(self, origin, radius1, radius2, thickness):
+        self.origin = np.array(origin, dtype="float")
+        self.r1 = radius1
+        self.r2 = radius2
+        self.thickness = thickness
+        # Distance between centres of circles
+        self.D = self.r1 + self.r2 - self.thickness
+        self.a = ((self.D) ** 2 + (self.r1) ** 2 - (self.r2) ** 2) / (
+            2 * self.D
+        )
+        self.b = self.D - self.a
+        self.h = (abs((self.r1) ** 2 - self.a**2)) ** 0.5
+        self.phi = math.degrees(2 * math.atan2(self.h, self.a))
+        self.theita = math.degrees(2 * math.atan2(self.h, self.b))
+
+    def draw(self):
+        lens_obj = []
+        i1 = self.a * np.array([1.0, 0.0])
+        i2 = self.b * np.array([1.0, 0.0])
+        # Surface 1 (Left surface: bulges left towards source)
+        lens_obj.append(
+            Mirror(
+                centre=self.origin + i1,
+                radius=self.r1,
+                aperture=self.phi,
+                facing=-1,
+                lens=True,
+            )
+        )
+        # Surface 2 (Right surface: bulges right away from source)
+        lens_obj.append(
+            Mirror(
+                centre=self.origin - i2,
+                radius=self.r2,
+                aperture=self.theita,
+                facing=1,
+                lens=True,
+            )
+        )
+        return lens_obj
+# The mirror object containing all mirror properties     
+
 
 # The ray object containing all ray properties and methods
 
 class Ray:
-    def __init__(self, origin, direction, mirrors, bounces=5, color="orange"):
+    def __init__(self, origin, direction, mirrors, bounces=10, color="orange"):
         self.origin = np.array(origin, dtype=float)
         self.direction = direction / np.linalg.norm(direction)
         self.length = 20
         self.mirrors: List[Mirror] = mirrors
-        self.bounces = bounces  #---Ray Bounce limit to prevent infinite bouncing
+        self.bounces = bounces  #---Ray Bounce limit to prevent infinite bouncingCCCCCC
         self.color = color      #---Ray Colour
         
         # Default parametric ray equation
@@ -78,7 +123,7 @@ class Ray:
                 t, mirror = min(self.points, key=lambda x: x[0])  # Finds point of intersection closest to ray, i.e the mirror which the ray intersects first, to prevent ghost rays
                 self.end = self.origin + t*self.direction
                 self.draw()
-                self.draw_reflection(mirror) 
+                self.draw_reflection_or_refraction(mirror) 
             else:
                 self.draw()
             return
@@ -102,17 +147,15 @@ class Ray:
         
     # Aperture Check function by checking parametric co-ordinate of circle range
     def parameter(self, pts, mirror):
-        for pt in pts:
-            intersection_point = self.origin + pt * self.direction
-            r = intersection_point - mirror.centre
-            
-            angle_deg = math.degrees(math.atan2(r[1], r[0]))
-            if angle_deg < 0:
-                angle_deg += 360                         # Normalizing the Angle
-
-            if mirror.centre_angle - mirror.angle/2 <= angle_deg <= mirror.centre_angle + mirror.angle/2:        # Checking if ray passes through aperture or just sphere of mirror
-                return pt
-        return None
+            for pt in pts:
+                intersection_point = self.origin + pt * self.direction
+                r = intersection_point - mirror.centre
+                # Fixed lower half of mirrror not working properly for concave curve(Angle Normalization Error)
+                angle_deg = math.degrees(math.atan2(r[1], r[0]))
+                diff = (angle_deg - mirror.centre_angle + 180) % 360 - 180
+                if abs(diff) <= mirror.angle / 2:
+                    return pt
+            return None
         
     # Drawing source rays
     def draw(self):
@@ -128,23 +171,60 @@ class Ray:
         )
         
     # Drawing Reflected Rays with recursive reflections
-    def draw_reflection(self, mirror):
-        incident_dir = self.direction 
+    def draw_reflection_or_refraction(self, mirror):
+        incident_dir = self.direction
         normal = mirror.normal(self.end)
         normal /= np.linalg.norm(normal)
-        if mirror.facing == -1:
-            normal *= -1
-        
-        dot = np.dot(incident_dir, normal)
-        reflect_dir = incident_dir - 2 * dot * normal   # Vector form of reflection, just flipping the component along the normal to maintain same angle but inverted direction.
-        
-        Ray(
-            origin=self.end, 
-            direction=reflect_dir, 
-            mirrors=self.mirrors, 
-            bounces=self.bounces - 1, 
-            color="blue"
-        )
+
+        # Standard Mirror Reflection
+        if not mirror.lens:
+            dot = np.dot(incident_dir, normal)
+            reflect_dir = incident_dir - 2 * dot * normal
+
+            Ray(
+                origin=self.end,
+                direction=reflect_dir,
+                mirrors=self.mirrors,
+                bounces=self.bounces - 1,
+                color="blue",
+            )
+
+        # Lens Refraction (Snell's Law)
+        else:
+            dot = np.dot(incident_dir, normal)
+
+            # Determine whether entering or exiting glass
+            if dot < 0:
+                n1, n2 = 1.0, 1.5  # Air -> Glass
+                oriented_normal = normal
+            else:
+                n1, n2 = 1.5, 1.0  # Glass -> Air
+                oriented_normal = -normal
+
+            n = n1 / n2
+            cos_i = -np.dot(incident_dir, oriented_normal)   # The dot product
+            sin2_t = (n**2) * (1.0 - cos_i**2)  # Used for checking total internal reflection(negative root case in the vector form of snell's law)
+
+            # Total Internal Reflection check
+            if sin2_t > 1.0:
+                refracted_dir = (
+                    incident_dir
+                    - 2 * np.dot(incident_dir, oriented_normal) * oriented_normal
+                )
+            else:
+                cos_t = math.sqrt(1.0 - sin2_t)
+                refracted_dir = (
+                    n * incident_dir + (n * cos_i - cos_t) * oriented_normal
+                )
+
+            Ray(
+                origin=self.end,
+                direction=refracted_dir,
+                mirrors=self.mirrors,
+                bounces=self.bounces - 1,
+                color="blue",
+            )
+
         
 # Defining the source object, main light emitter.
 class Source:
@@ -182,46 +262,108 @@ def draw_scene():
     Source(
         origin=source_pos,
         mirrors=mirrors,
-        rays=18
+        rays=50
     )
 
     ax.legend(loc="upper right")
     fig.canvas.draw()
 
 
+# Mouse + Keyboard  Input Handler
+def close_input():
+    global box_ax, textbox
+    if box_ax is not None:
+        box_ax.remove()
+        box_ax = None
+        textbox = None
+        fig.canvas.draw_idle()
+
+def create_textbox(event, label, callback):
+    global box_ax, textbox
+    if box_ax is not None:
+        box_ax.remove()
+
+    fig_x, fig_y = fig.transFigure.inverted().transform((event.x, event.y))
+    box_width = 0.3
+    box_height = 0.05
+    fig_x = min(fig_x, 1 - box_width)
+    fig_y = min(fig_y, 1 - box_height)
+
+    box_ax = fig.add_axes([fig_x, fig_y, box_width, box_height])
+    textbox = TextBox(box_ax, label)
+    textbox.on_submit(callback)
+    fig.canvas.draw_idle()
+
 # Mouse Input handler
 def on_click(event):
-    if event.inaxes is None:
+    global box_ax, textbox
+
+    if event.inaxes is None or (box_ax is not None and event.inaxes is box_ax):
         return
 
     # Assigning Left click for moving source
     if event.button == 1:
+        if box_ax is not None:
+            close_input()
         source_pos[:] = [event.xdata, event.ydata]
         draw_scene()
 
-    # Assigning Right Click for placing mirror
+    # Assigning Right Click for placing mirror or lens
     elif event.button == 3:
         pole = np.array([event.xdata, event.ydata])
 
-        radius = 6
+        # Holding control to place lens
+        if event.key and "control" in event.key.lower():
+            values = []
+            prompts = [
+                "Radius 1 (Concave): ",
+                "Radius 2 (Convex): ",
+                "Thickness of Lens: ",
+            ]
 
-        # Holding shift to place mirror
-        facing = -1 if event.key == "shift" else 1
+            def submit_lens(text):
+                try:
+                    values.append(float(text))
+                except ValueError:
+                    textbox.set_val("")
+                    return
 
-        centre = pole - np.array([facing * radius, 0])
+                if len(values) < len(prompts):
+                    create_textbox(event, prompts[len(values)], submit_lens)
+                else:
+                    lens = Lens(pole, values[0], values[1], values[2])
+                    lens_helper_mirror = lens.draw()
+                    mirrors.extend(
+                        lens_helper_mirror
+                    )
+                    close_input()
+                    draw_scene()
 
-        mirrors.append(
-            Mirror(
-                centre=centre,
-                radius=radius,
-                aperture=70,
-                facing=facing
-            )
-        )
+            create_textbox(event, prompts[0], submit_lens)
 
-        draw_scene()
+        # All mirror placements ask for radius
+        else:
+            facing = -1 if (event.key and "shift" in event.key.lower()) else 1
 
+            def submit_mirror(text):
+                try:
+                    radius = float(text)
+                except ValueError:
+                    textbox.set_val("")
+                    return
 
+                centre = pole - np.array([facing * radius, 0])
+                mirrors.append(
+                    Mirror(
+                        centre=centre,
+                        radius=radius,
+                        aperture=70,
+                        facing=facing,
+                    )
+                )
+                close_input()
+                draw_scene()
+            create_textbox(event, "Radius: ", submit_mirror)
 # Keyboard handler
 def on_key(event):
     if event.key.lower() == "c":
